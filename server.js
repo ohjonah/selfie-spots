b2ca843f8266d53e521e242978e9bf4ad30e6135
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-07-01 10:47:31 -0700

    Adds server route to relay Instagram API calls.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -13,5 +15,12 @@ app.get('/', function (request, response) {
 app.get('/home', function (request, response) {
   response.sendFile('index.html', {root: './public'});
 });
+
+app.get('/ig/*', requestProxy({
+    url: 'https://api.instagram.com/v1/*',
+    query: {
+      access_token: process.env.INSTAGRAM_TOKEN
+    }
+}));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
