4d0068144240adc97e8bbfeced6b6e8fe4d5aebb
Nick Asmussen <nick.asmussen1@gmail.com>
2017-07-06 12:17:51 -0700

    makes a delete ajax call to remove a fav, styles the favorites page, hooks up the mobile menu handler

--- public/scripts/views/favoritesView.js
+++ public/scripts/views/favoritesView.js
@@ -5,7 +5,7 @@ var app = app || {};
 (function(module) {
 
   var favoritesView = {};
-  
+
   favoritesView.index = function(favorites) {
     let $favorites = $('#favorites');
     $('section').addClass('hidden');
@@ -13,6 +13,19 @@ var app = app || {};
 
     let template = Handlebars.compile($('#favorite-template').html());
     favorites.forEach(f => $favorites.append(template(f)));
+
+    $favorites.find('.favorite').find('a').on('click', function(e) {
+      e.preventDefault();
+      $.ajax({
+        method: "DELETE",
+        url: `users/${app.User.id}/favorites`,
+        data: {
+          location_id: $(this).data('id')
+        }
+      }).done(() => {
+        $(this).parent().remove();
+      });
+    });
   };
 
   module.favoritesView = favoritesView;
